37bde89a0a2c3d6da139c236f78eff822e9fce96
st01tyy <44644324+st01tyy@users.noreply.github.com>
2020-06-29 21:07:24 +0800

    Initial commit

---
+++ src/main/java/edu/bistu/kshost/KshostApplication.java
@@ -0,0 +1,13 @@
+package edu.bistu.kshost;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KshostApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KshostApplication.class, args);
+    }
+
+}
---
+++ src/test/java/edu/bistu/kshost/KshostApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.bistu.kshost;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KshostApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
